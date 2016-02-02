0d695c14f43e4e71f8453f5aa4ef609feba03397
ysadka <ysadka@gmail.com>
2016-02-02 11:29:51 -0800

    edit, create, delete wineries

--- app/controllers/wineries_controller.rb
+++ app/controllers/wineries_controller.rb
@@ -1,26 +1,46 @@
 class WineriesController < ApplicationController
 
 	def new
+		@winery = Winery.new
 	end
 
 	def create
+		@winery = Winery.new(winery_params)
+		if @winery.save
+      flash[:success] = "Winery Added"
+      redirect_to root_path
+    else
+      render 'new'
+		end
 	end
 
 	def show
+		@winery = Winery.find(params[:id])
 	end
 
 	def edit
+		@winery = Winery.find(params[:id])
 	end
 
 	def update
+		@winery = Winery.find(params[:id])
+		if @winery.update_attributes(winery_params)
+			redirect_to root_path
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
+		@winery = Winery.find(params[:id])
+		@winery. destroy
+    flash[:success] = "Winery deleted"
+    redirect_to root_path
 	end
 
 	private
-	
+
 	def winery_params
-		params.require(:winery).permit(:name, :wine_club)		
+		params.require(:winery).permit(:name, :address, :phone)
 	end
 end

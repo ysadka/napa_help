25ec2406df7e1fff9ee276c4e154e3b3c61d3d02
ysadka <ysadka@gmail.com>
2016-02-05 20:00:46 -0800

    remove to seed db

--- app/models/winery.rb
+++ app/models/winery.rb
@@ -3,9 +3,9 @@ class Winery < ActiveRecord::Base
 	has_and_belongs_to_many :varietals
 
 	validates :address,  presence: true
-	validates :city, 		 presence: true
+	# validates :city, 		 presence: true
 	validates :name, 		 presence: true
 	validates :phone,  	 presence: true
 	validates :state, 	 presence: true
-	validates :website,  presence: true
+	# validates :website,  presence: true
 end
